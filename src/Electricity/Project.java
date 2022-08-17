0e04f381417e3238b7a0f7b715cd93b5c34006d6
zariq <ahmadnazariq@gmail.com>
2022-08-17 23:20:39 +0800

    try

--- src/Electricity/Project.java
+++ src/Electricity/Project.java
@@ -13,7 +13,7 @@ public class Project extends JFrame implements ActionListener{
         setSize(1920,1030);
         
         /* Adding background image */
-        //testing testing
+        //testing testing doneSSS
 
         ImageIcon ic =  new ImageIcon(ClassLoader.getSystemResource("icon/elect1.jpg"));
         Image i3 = ic.getImage().getScaledInstance(1900, 950,Image.SCALE_DEFAULT);
